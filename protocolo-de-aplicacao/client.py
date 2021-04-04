d975ff6543347a442fa2cf8cabf56f5651763ca7
Marcelo Bittencourt <35068186+marcelo-bn@users.noreply.github.com>
2021-04-04 14:08:46 -0300

    Add files via upload

--- protocolo-de-aplicacao/client.py
+++ protocolo-de-aplicacao/client.py
@@ -66,43 +66,38 @@ def requisicao_prova(token, idProva):
     return resposta
 
 # Envia respostas
-def enviando_resposta(token, idProva):
+def enviando_resposta(lista_idQuestao, lista_alternativas, toke, idProva):
     id_questao = [1,2]
     id_alternativas = [['v3p1'],['v4p1','v5p1']]
-
-    # Questao 1 
-    #cod1 = mensagem_pb2.CODIGOS()
-    #cod1.codigos.append('v3p1')
-    rsp1 = mensagem_pb2.RESPOSTA()
-    rsp1.id = 1
-    rsp1.codigos.codigos.append('v3p1')
-
-    # Questao 2
-    #cod2 = mensagem_pb2.CODIGOS()
-    #cod2.codigos.append('v4p1')
-    #cod2.codigos.append('v5p1')
-    rsp2 = mensagem_pb2.RESPOSTA()
-    rsp2.id = 2
-    rsp2.codigos.codigos.append('v4p1')
-    rsp2.codigos.codigos.append('v5p')
 
     # Mensagem REQ_RESP
     resposta_prova = mensagem_pb2.REQ_RESP()
     resposta_prova.token = token
     resposta_prova.id_prova = idProva
-    resposta_prova.respostas.append(rsp1)
-    resposta_prova.respostas.append(rsp2)
 
+    # Lista de RESPOSTAS
+    lista_msg_resposta = []
+
+    for r in range(0,len(lista_idQuestao)):      
+        rsp = mensagem_pb2.RESPOSTA()            # Cria objeto resposta
+        rsp.id = lista_idQuestao[r]              # Armazena a questão da lista_idQuestao
+        for alt in lista_alternativas[r]:
+            rsp.codigos.codigos.append(alt)     # Arnazena as alternativas escolhidas dessa questão
+        
+        resposta_prova.respostas.append(rsp)     # Armazena um mensagem RESPOSTA em REQ_RESP
+
+    # Mensagem do tipo MENSAGEM
     msg = mensagem_pb2.MENSAGEM()
-    msg.reqresp.token = resposta_prova.token
-    msg.reqresp.id_prova = resposta_prova.id_prova 
-    msg.reqresp.respostas.append(resposta_prova.respostas[0]) 
-    msg.reqresp.respostas.append(resposta_prova.respostas[1]) 
+    msg.reqresp.token = resposta_prova.token 
+    msg.reqresp.id_prova = resposta_prova.id_prova
+    for i in range(0,len(resposta_prova.respostas)):
+        msg.reqresp.respostas.append(resposta_prova.respostas[i])     # Armazenando as mensagens do tipo REQ_RESP
+
+    #print(msg)
     data = msg.SerializeToString()
-
     resposta = envia(data)
-
-    return resposta
+    
+    return True
 
 # Requisiçao do resultado
 def requisicao_resultado(token, idProva):
@@ -135,23 +130,48 @@ if __name__ == "__main__":
 
         print("Seu token é: "+token)
         auth = True
+        lista_idQuestao = []
+        lista_alternativas = []
 
         while auth:
             op = -1
             print('> 1 - Iniciar prova.')
             print('> 2 - Sair da sua conta.')
-            op = int(input('> O que você deseja? '))
+            op = int(input('> Insira uma opção: '))
 
             if op == 1:
-                idProva = input('> Insira o id da prova fornecido pelo seu professor.')
-                r = requisicao_prova(token, idProva)
-                print("Prova: ")
-                print(r.ackreqprova)
 
-                op1 = int(input('> Enviar repostas? SIM [1], NÃO[2] '))
+                idProva = input('> Insira o id da prova fornecido pelo seu professor: ')
+                r = requisicao_prova(token, idProva)
+                print("\n")
+
+                # Apresenta questões e alternativas
+                for q in r.ackreqprova.questoes:
+                    print(q.id,"-",q.enunciado,"(",q.pontos,"pontos)")
+                    lista_idQuestao.append(q.id)
+                    for a in q.alternativas:
+                        print(a.codigo,"-",a.descricao)
+
+                print("\n> Responda as questões: ")    
+                # Armazena respostas
+                for q in r.ackreqprova.questoes:
+                    lista_aux_alternativas =[]
+                    print("> Questão",q.id)
+                    o = ""
+                    while o!="sair":
+                        o = input("> Digite o código da alternativa ou [sair] para ir para a próxima questão: ")
+                        if o != "sair":
+                            lista_aux_alternativas.append(o)
+                    lista_alternativas.append(lista_aux_alternativas)
+
+                print(lista_idQuestao)
+                print(lista_alternativas)
+
+                op1 = int(input('\n> Enviar repostas? SIM [1], NÃO[2]: '))
+
                 if op1 == 1:
-                    enviando_resposta(token, idProva)
-                    op2 = int(input('> Deseja receber o resultado? SIM [1], NÃO[2] '))
+                    enviando_resposta(lista_idQuestao, lista_alternativas, token, idProva)
+                    op2 = int(input('> Deseja receber o resultado? SIM [1], NÃO[2]: '))
                     if op2 == 1:
                         r = requisicao_resultado(token, idProva)
                         print(r)
